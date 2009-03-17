7daa40876c88ed756c5b4696cfb54f95d3a43a07
Pedro Paulo <pedropaulovc@gmail.com>
2009-03-17 01:00:28 +0000

    Teste 2

---
+++ tags/MediaUfsc.java
@@ -0,0 +1,16 @@
+public class MediaUfsc {
+	/**
+	 * @author Pedro Paulo Vezzá Campos - 0132A
+	 */
+	public static void main(String[] args) {
+		float nota1 = 7.5f;
+		float nota2 = 8.0f;
+		float nota3 = 5.5f;
+		
+		float media = (nota1+nota2+nota3)/3;
+		
+		System.out.println(media); 
+
+	}
+
+}
