e5f0a573a08e4e8b856c19a1c46b0b9a364c94cd
Pedro Paulo <pedropaulovc@gmail.com>
2009-10-11 22:10:06 +0000

    Reorganizados pacotes, separado acervo de biblioteca

--- BibliotecaHistorias/src/producao/acervo/TipoAcervo.java
+++
@@ -1,9 +0,0 @@
-package producao.acervo;
-
-public interface TipoAcervo<Tipo> {
-
-	public int tamanho();
-	public boolean adicionar(Tipo t);
-	public Tipo remover(int i);
-	public Tipo obter(int i);
-}
