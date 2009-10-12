7805f9073f55f70fd810ad8e5817e0a7f95a5b3a
Pedro Paulo <pedropaulovc@gmail.com>
2009-10-12 00:36:26 +0000

    bug fix acervo.java

--- BibliotecaHistorias/src/acervo/producao/Acervo.java
+++ BibliotecaHistorias/src/acervo/producao/Acervo.java
@@ -3,31 +3,31 @@ package acervo.producao;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Acervo<Arquivavel> implements TipoAcervo<Arquivavel> {
+public class Acervo<Tipo> implements TipoAcervo<Tipo> {
 
-	private List<Arquivavel> acervo;
+	private List<Tipo> acervo;
 
 	public Acervo() {
-		acervo = new ArrayList<Arquivavel>();
+		acervo = new ArrayList<Tipo>();
 	}
 
 	public int tamanho() {
 		return acervo.size();
 	}
 
-	public boolean adicionar(Arquivavel t) {
+	public boolean adicionar(Tipo t) {
 		return acervo.add(t);
 	}
 
-	public Arquivavel obter(int i) {
+	public Tipo obter(int i) {
 		return acervo.get(i);
 	}
 
-	public Arquivavel remover(int i) {
+	public Tipo remover(int i) {
 		return acervo.remove(i);
 	}
 
-	public List<Arquivavel> obterLista(){
+	public List<Tipo> obterLista(){
 		return acervo;
 	}
 
