3ae994a74b27c77e55c5bf455c90633263a6ebc4
Pedro Paulo <pedropaulovc@gmail.com>
2010-03-28 01:34:32 +0000

    Criado gerarMatrizHilbert

--- CalcNum/SolverSistemaLinear/src/Escalonador.java
+++ CalcNum/SolverSistemaLinear/src/Escalonador.java
@@ -6,7 +6,7 @@ public class Escalonador {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		float[][] matriz = obterMatriz();
+		float[][] matriz = gerarMatrizHilbert(10);
 		System.out.println("INÍCIO: ");
 		exibirMatriz(matriz);
 		System.out.println("Escalonando: ");
@@ -15,8 +15,8 @@ public class Escalonador {
 		exibirMatriz(matriz);
 	}
 
-	private static void escalonarMatriz(float [][] matriz) {
-		for(int colPrinc = 0; colPrinc < matriz.length - 1; colPrinc++){
+	private static void escalonarMatriz(float[][] matriz) {
+		for (int colPrinc = 0; colPrinc < matriz.length - 1; colPrinc++) {
 			pivotarLinhas(matriz, colPrinc);
 			System.out.println("Pivotado coluna " + colPrinc);
 			exibirMatriz(matriz);
@@ -32,20 +32,19 @@ public class Escalonador {
 	private static void pivotarLinhas(float[][] matriz, int linhaAtual) {
 		float maior = 0;
 		int linhaMaior = linhaAtual;
-		
-		for(int i = linhaAtual; i < matriz.length; i++){
-			if(matriz[i][linhaAtual] > maior){
+
+		for (int i = linhaAtual; i < matriz.length; i++) {
+			if (matriz[i][linhaAtual] > maior) {
 				maior = matriz[i][linhaAtual];
 				linhaMaior = i;
 			}
 		}
-		
-		if(maior == 0){
+
+		if (maior == 0) {
 			System.out.println("Sistema SPI ou SI");
 			System.exit(1);
 		}
-			
-		
+
 		float[] temp = matriz[linhaAtual];
 		matriz[linhaAtual] = matriz[linhaMaior];
 		matriz[linhaMaior] = temp;
@@ -53,17 +52,18 @@ public class Escalonador {
 
 	private static void dividirLinha(float[][] matriz, int indice) {
 		float fatorDivisao = matriz[indice][indice];
-		for(int i = indice; i < matriz[0].length; i++){
-			matriz[indice][i] = matriz[indice][i]/fatorDivisao;
+		for (int i = indice; i < matriz[0].length; i++) {
+			matriz[indice][i] = matriz[indice][i] / fatorDivisao;
 		}
-			
+
 	}
 
 	private static void subtrairLinhaDeOutra(float[][] matriz, int indice) {
-		for(int i = indice + 1; i < matriz.length; i++){
+		for (int i = indice + 1; i < matriz.length; i++) {
 			float fatorCondenado = matriz[i][indice];
-			for(int j = indice; j < matriz[0].length; j++){
-				matriz[i][j] = matriz[i][j] - fatorCondenado * matriz[indice][j];
+			for (int j = indice; j < matriz[0].length; j++) {
+				matriz[i][j] = matriz[i][j] - fatorCondenado
+						* matriz[indice][j];
 			}
 		}
 	}
@@ -71,7 +71,7 @@ public class Escalonador {
 	private static void exibirMatriz(float[][] matriz) {
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[0].length; j++)
-				System.out.printf("%8.2f ", matriz[i][j]);
+				System.out.printf("%15.12f ", matriz[i][j]);
 			System.out.println();
 		}
 		System.out.println();
@@ -90,6 +90,16 @@ public class Escalonador {
 			}
 		}
 
+		return matriz;
+	}
+
+	private static float[][] gerarMatrizHilbert(int ordem) {
+		float[][] matriz = new float[ordem][ordem];
+
+		for (int i = 1; i <= ordem; i++)
+			for (int j = 1; j <= ordem; j++)
+				matriz[i - 1][j - 1] = 1.0f/(i + j - 1);
+				
 		return matriz;
 	}
 
