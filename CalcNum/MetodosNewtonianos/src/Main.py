6c43a5f7154565657583a7049dfed70e5d4574d5
Pedro Paulo <pedropaulovc@gmail.com>
2010-05-15 16:37:56 +0000

    Arrumado newtonGeral, falta testar.

--- CalcNum/MetodosNewtonianos/src/Main.py
+++ CalcNum/MetodosNewtonianos/src/Main.py
@@ -4,32 +4,33 @@ Created on 15/05/2010
 
 @author: PedroPaulo
 '''
-from newtonianos import *
+from newtonianos import newtonGeral, secante, steffensen
 
 def main():
 	metodo = 0
 	while(metodo > 3 or metodo < 1):
-		print "Escolha o método que deseja utilizar"
-		print "1 - Newton Geral"
-		print "2 - Secante"
-		print "3 - Steffensen"
+		print 'Escolha o método que deseja utilizar'
+		print '1 - Newton Geral'
+		print '2 - Secante'
+		print '3 - Steffensen'
 		metodo = int(raw_input())
 		if(metodo > 3 or metodo < 1):
-			print "Opção inválida"
+			print 'Opção inválida'
 
-	func = raw_input("Forneça a função desejada: ")
+	func = raw_input('Forneça a função desejada: ')
 
-	xk = int(raw_input("Forneça o primeiro valor"))
+	xk = float(raw_input('Forneça o primeiro valor: '))
 
+	precisao = float('1e-' + raw_input('Forneça a precisão em casas decimais: '));
 	if(metodo == 1):
-		dfunc = raw_input("Forneça a derivada da função anterior: ")
-		newtonGeral(func, dfunc, xk)
+		dfunc = raw_input('Forneça a derivada da função anterior: ')
+		newtonGeral(func, dfunc, precisao, xk)
 	elif(metodo == 2):
-		xkPost = int(raw_input("Forneça o segundo valor"))
-		secante(func, xk, xkPost)
+		xkPost = int(raw_input('Forneça o segundo valor'))
+		secante(func, xk, precisao, xkPost)
 	else:
-		steffensen(func, xk)
+		steffensen(func, precisao, xk)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
--- CalcNum/MetodosNewtonianos/src/newtonianos.py
+++ CalcNum/MetodosNewtonianos/src/newtonianos.py
@@ -7,51 +7,44 @@ Createdon15/05/2010
 def f(func, x):
 	return float(eval(func))
 
-def df(func_derivada, x):
-	return float(eval(func_derivada))
+def newtonGeral(func, dfunc, precisao, xk):
+	den = f(dfunc, xk)
 
-def newtonGeral(func, dfunc, xk):
-	print "Newton Geral"
-#	den = df(x)
-#	if(den == 0)
-#		print "Divisão por zero na iteração inicial"
-#		return;
-#
-#	xkPost = xk - funcao.obterValor(xk) / den;
-#	intnumIteracoes = 0;
-#	doubleerroV = abs(xkPost - xk) / max(abs(xk), abs(xkPost));
-#	xk = xkPost;
-#	
-#	booleanerroDiminuindo = true;
-#	while(erroDiminuindo){
-#	numIteracoes + +;
-#	den = funcao.obterValorDerivada(xk);
-#	if(den == 0){
-#	System.out.println("Divis�oporzero");
-#	return;
-#	}
-#	
-#	xkPost = xk - funcao.obterValor(xk) / den;
-#	erroN = abs(xkPost - xk) / max(abs(xk), abs(xkPost));
-#	
-#	if(erroN <= precisao){
-#	System.out.println("Solu��oobtidacom" + numIteracoes
-#	+ "itera��es:" + xkPost);
-#	return;
-#	}
-#	
-#	if(erroN >= erroV)
-#	erroDiminuindo = false;
-#	
-#	xk = xkPost;
-#	erroV = erroN;
-#	}
-#	System.out
-#	.println("M�todopodeestardivergindo.N�merodeitera��es:"
-#	+ numIteracoes);
+	if(den == 0):
+		print 'Divisão por zero na iteração inicial'
+		return
 
-def secante(func, xk, xkPost):
-	print "Lorem"
+	xkPost = xk - f(func, xk) / den
+	numIteracoes = 0
+	erroV = abs(xkPost - xk) / max(abs(xk), abs(xkPost))
+	xk = xkPost
+
+	erroDiminuindo = True
+	while(erroDiminuindo):
+		numIteracoes += 1
+		den = f(dfunc, xk);
+		if(den == 0):
+			print 'Divisão por zero'
+			return
+		
+
+		xkPost = xk - f(func, xk) / den
+		erroN = abs(xkPost - xk) / max(abs(xk), abs(xkPost))
+
+		if(erroN <= precisao):
+			print 'Solução obtida com {0} iterações: {1}'.format(numIteracoes, xkPost)
+			return
 	
-def steffensen(func, xk):
-	print "Ipsum"
+		if(erroN >= erroV):
+			erroDiminuindo = False
+
+		xk = xkPost
+		erroV = erroN
+	
+	print 'Método pode estar divergindo. Iterações: {0}'.format(numIteracoes)
+
+def secante(func, xk, precisao, xkPost):
+	print 'Lorem'
+
+def steffensen(func, precisao, xk):
+	print 'Ipsum'
