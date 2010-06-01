356d1b385487aa7521de786432502a8d608fd4e5
Felipe Silveira <felipessilveira@gmail.com>
2010-06-01 12:33:41 +0000

    leitura do arquivo e separação das substrings

--- ED/ArvoreAVL/Main.cpp
+++ ED/ArvoreAVL/Main.cpp
@@ -1,6 +1,43 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string* lerCidade(std::string linha){
+	size_t posicaoBarra, posicaoInicioCep;
+	string cidade, cep;
+	posicaoBarra = linha.find('|');
+	if(posicaoBarra != string::npos){
+		cidade = linha.substr(0,posicaoBarra);
+		cep = linha.substr(posicaoBarra+1);
+		posicaoInicioCep = cep.find_first_not_of("	");
+		cep = cep.substr(posicaoInicioCep);
+		cout << "cidade:" << cidade << endl << "cep:" << cep << endl;
+	}
+}
+
+
+void lerArquivo() {
+	string line;
+	ifstream myfile("dados.txt");
+	if (myfile.is_open()) {
+		while (!myfile.eof()) {
+			getline(myfile, line);
+			lerCidade(line);
+		}
+		myfile.close();
+	}
+
+	else
+		std::cout << "Unable to open file";
+
+}
 
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	//::testing::InitGoogleTest(&argc, argv);
+	//return RUN_ALL_TESTS();
+
+	lerArquivo();
+	return 0;
 }
