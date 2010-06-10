fb6c7001268143cfceefc45bfe713b18c35d4a62
Pedro Paulo <pedropaulovc@gmail.com>
2010-06-10 16:25:30 +0000

    Método de Simpson composto feito

--- CalcNum/MetodoSimpson/src/main.py
+++ CalcNum/MetodoSimpson/src/main.py
@@ -6,11 +6,21 @@ Created on Jun 9, 2010
 @author: pedropaulo
 '''
 from metodoSimpson import *
+from time import time
 
 def main():
-	precisao = precisaoMaquina()
 
-	print simpsonComposto('sqrt(x)', 1.0, 2.0, precisao, 2.0)
+	print integrarSimpson('sqrt(x) ', 1.0, 2.0, 8.0)
+	print integrarSimpson('sqrt(x) ', 2.0, 1.0, 8.0)
+
+	tempo1 = time()
+	simpsonComposto('sqrt(x) ', 1.0, 2.0, 10**-10 , 2.0)
+	tempo2 = time()
+	simpsonCompostoModificado('sqrt(x)', 1.0, 2.0, 10**-10 , 2.0)
+	tempo3 = time()
+	
+	print 'Tempo composto normal = ', tempo2 - tempo1
+	print 'Tempo composto modificado = ', tempo3 - tempo2
 
 
 def precisaoMaquina():
--- CalcNum/MetodoSimpson/src/metodoSimpson.py
+++ CalcNum/MetodoSimpson/src/metodoSimpson.py
@@ -7,17 +7,31 @@ Created on Jun 9, 2010
 from math import *
 
 def f(func, x):
+	'''Calcula o valor de func no ponto x'''
 	return float(eval(func))
+
+def buscarValorF(func, x, pontosF):
+	''' 
+	Caso o valor de func(x) já esteja armazenado em pontosF, reaproveitar,
+	se não, calcular e salvar em pontosF
+	'''
+	if (x in pontosF):
+		return pontosF[x]
+	novoPonto = f(func, x)
+	pontosF[x] = novoPonto
+	return novoPonto
 
 
 def integrarSimpson(func, inicio, fim, partes):
+	''' 
+	Integra 'func' de 'inicio' até 'fim', dividindo o intervalo em 'partes' partes.
+	Instável.
+	'''
 	inicio = float(inicio)
 	fim = float(fim)
 
-
 	h = (fim - inicio) / partes
 	extremos = f(func, inicio) + f(func, fim)
-
 
 	partes2 = int(partes / 2)
 	h2 = h + h
@@ -35,7 +49,38 @@ def integrarSimpson(func, inicio, fim, partes):
 
 	return (h / 3) * (extremos + 4 * somaPares + 2 * somaImpares)
 
+def integrarSimpsonModificado(func, inicio, fim, partes, pontosF):
+	''' 
+	Integra 'func' de 'inicio' até 'fim', dividindo o intervalo em 'partes' partes.
+	Reaproveita pontos já calculados. Instável.
+	'''
+	inicio = float(inicio)
+	fim = float(fim)
+
+	h = (fim - inicio) / partes
+	extremos = buscarValorF(func, inicio, pontosF) + buscarValorF(func, fim, pontosF)
+
+	partes2 = int(partes / 2)
+	h2 = h + h
+	c = inicio + h
+	somaPares = buscarValorF(func, c, pontosF)
+	for _ in range(1, partes2):
+		c += h2
+		somaPares += buscarValorF(func, c, pontosF)
+
+	somaImpares = 0.0
+	c = inicio
+	for _ in range(1, partes2):
+		c += h2
+		somaImpares += buscarValorF(func, c, pontosF)
+
+	return (h / 3) * (extremos + 4 * somaPares + 2 * somaImpares)
+
 def simpsonComposto(func, inicio, fim, precisao, partes):
+	''' 
+	Integra usando o método de Simpson composto até 'precisao' desejada. 
+	Estável.
+	'''
 	int = integrarSimpson(func, inicio, fim, partes)
 	intAnterior = int
 	erro = 10 * precisao
@@ -52,7 +97,46 @@ def simpsonComposto(func, inicio, fim, precisao, partes):
 			intParcial = integrarSimpson(func, c, d, partes)
 			int += intParcial
 			c = d
-		erro = abs(int - intAnterior) / max(abs(int), abs(intAnterior))
+			num = abs(int - intAnterior)
+			if(num == 0):
+				erro = 0
+			else:
+				erro = num / max(abs(int), abs(intAnterior))
+		intAnterior = int
+		passo /= 2
+		k = k + k
+		numIteracoes += 1
+
+	return int
+
+def simpsonCompostoModificado(func, inicio, fim, precisao, partes):
+	''' 
+	Integra usando o método de Simpson composto até 'precisao' desejada. 
+	Reaproveita pontos já calculados. Estável. Aproximadamente 2X mais rápido que simpsonComposto
+	'''
+	pontosF = dict()
+
+	int = integrarSimpsonModificado(func, inicio, fim, partes, pontosF)
+	intAnterior = int
+	erro = 10 * precisao
+	passo = (fim - inicio) / 2
+	k = 2
+
+	numIteracoes = 1
+	while(erro > precisao):
+		c = inicio
+		int = 0
+
+		for _ in range(k):
+			d = c + passo
+			intParcial = integrarSimpsonModificado(func, c, d, partes, pontosF)
+			int += intParcial
+			c = d
+			num = abs(int - intAnterior)
+			if(num == 0):
+				erro = 0
+			else:
+				erro = num / max(abs(int), abs(intAnterior))
 		intAnterior = int
 		passo /= 2
 		k = k + k
