889dcf5d6fbe59c6ea1c4e8b253dc585aaea8ce5
Pedro Paulo <pedropaulovc@gmail.com>
2010-07-04 21:49:20 +0000

    Limpeza no código do avião

--- ProgConc/Aeroporto/src/objetos/Aviao.java
+++ ProgConc/Aeroporto/src/objetos/Aviao.java
@@ -19,22 +19,21 @@ public class Aviao extends Thread {
 		this.idTorre = idTorre;
 		this.caixaPostal = caixaPostal;
 		emSolo = new Random().nextBoolean();
-		
-		if(emSolo)
+
+		if (emSolo)
 			Log.adicionarLog("Avião " + id + " em solo", 0);
 		else
 			Log.adicionarLog("Avião " + id + " voando", 0);
 	}
 
-	public void run() {		
+	public void run() {
 		while (true) {
 			Mensagem msg = new Mensagem();
 			CodigosMensagem codigo;
-			if (emSolo){
+			if (emSolo) {
 				codigo = REQUISICAO_DECOLAGEM;
 				dormir(0);
-			}
-			else{
+			} else {
 				codigo = REQUISICAO_POUSO;
 				dormir(new Random().nextInt(2300));
 			}
@@ -43,32 +42,21 @@ public class Aviao extends Thread {
 			caixaPostal.send(idTorre, msg);
 
 			msg = caixaPostal.receive(id);
-			switch (msg.obterCodigo()) {
-			case OPERACAO_AUTORIZADA:
+
+			if (msg.obterCodigo() == OPERACAO_AUTORIZADA) {
 				Log.adicionarLog("Avião " + id + " foi autorizado", 1);
 				emSolo = !emSolo;
-//				msg.definirCodigo(OPERACAO_CONCLUIDA);
-//				msg.definirId(id);
-//				caixaPostal.send(idTorre, msg);
-				break;
 			}
-			
 		}
 	}
-	
-	private void dormir(int tempo) {
 
+	private void dormir(int tempo) {
 		try {
-			sleep(500+tempo);
+			sleep(500 + tempo);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}
-
-	public boolean estaEmSolo(){
-		return emSolo;
 	}
 
 	public int obterId() {
