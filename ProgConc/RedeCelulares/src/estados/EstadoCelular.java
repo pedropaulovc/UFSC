fd1f5524945524fbf6b00402f7c72eb6afdd1939
Rafael Pedretti <rafael.pedretti@gmai.com>
2010-07-05 19:05:08 +0000

    limpeza no trunk

--- ProgConc/RedeCelulares/src/estados/EstadoCelular.java
+++
@@ -1,5 +0,0 @@
-package estados;
-
-public enum EstadoCelular {
-	LIVRE, EM_LIGACAO, MANDANDO_MENSAGEM, DESLIGADO, TENTANDO_LIGACAO
-}
--- ProgConc/RedeCelulares/src/mensagem/CodigosErro.java
+++
@@ -1,7 +0,0 @@
-package mensagem;
-
-public enum CodigosErro {
-	//Códigos de erro entre celular e estação
-	NAO_ASSOCIADO, NAO_DESASSOCIADO, DESTINO_INDISPONIVEL
-	
-}
