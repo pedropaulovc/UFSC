db1af2b8b60e182957d77ebe1cd106a4ba960173
Felipe Silveira <felipessilveira@gmail.com>
2010-07-10 17:43:40 +0000

    mudanças no script e o arquivo de portarias pronto

--- ED/ListaInvertida/Indices/gerar_dat.sh
+++ ED/ListaInvertida/Indices/gerar_dat.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
 IFS='\n'
+arquivoDat="portarias.dat"
 
 for file in *
 do
     if echo "$file" | grep ".txt" > /dev/null
     then
-	iconv --from-code=ISO-8859-15 --to-code=UTF-8 "$file" >> temp && mv temp "$file"
+	if file "$file" | grep "ISO-8859" > /dev/null
+	then
+	    iconv --from-code=ISO-8859-15 --to-code=UTF-8 "$file" >> temp && mv temp "$file"
+	fi
 
-        cat "$file" | sed -n -e "/PORTARIA Nº/,$"p | \
-                      sed -e s/".*\(PORTARIA Nº.*\)"/"\1"/ \
+	nomeReal=$(echo "$file" | sed s/"\.txt"/""/)
+	echo -n "$nomeReal|" >> "$arquivoDat"
+
+        cat "$file" | sed -n -e "/PORTARIAS/,$"p | \
+                      sed -e s/'PORTARIAS'/''/ \
                           -e s/"[áÁàÀâÂãÃ]"/"a"/g \
                           -e s/"[éÉêÊ]"/"e"/g \
                           -e s/"[íÍ]"/"i"/g \
                           -e s/"[óÓôÔõÕ]"/"o"/g \
                           -e s/"[úÚüÜ]"/"u"/g \
                           -e s/"[çÇ]"/"c"/g \
-	                  -e s/"\&quot;"/"\""/g
+	                  -e s/"\&quot;"/"\""/g | \
+	tr [:lower:] [:upper:] | tr [:space:] ' ' >> "$arquivoDat"
+	echo -ne "|\n" >> "$arquivoDat"
    fi
 done
+
+numeroLinhas=$(wc -l "$arquivoDat" | sed s/"[^0-9]"/""/g)
+sed 1i\ "$numeroLinhas" "$arquivoDat" > temp && mv temp "$arquivoDat"
+
 
 
