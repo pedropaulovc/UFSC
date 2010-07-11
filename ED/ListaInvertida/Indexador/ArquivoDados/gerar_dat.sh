c11b1d813308755707eb0010c15d0969b282014e
Felipe Silveira <felipessilveira@gmail.com>
2010-07-11 20:38:31 +0000

    documentacao do script

--- ED/ListaInvertida/Indexador/ArquivoDados/gerar_dat.sh
+++ ED/ListaInvertida/Indexador/ArquivoDados/gerar_dat.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
+# Script criado para facilitar o trabalho de criar o arquivo .dat
+# Motivacao: dificuldade de trabalhar com strings em c++ por causa
+#            da codificacao dos caracteres dos arquivos .txt
+ 
+
 IFS='\n'
 arquivoDat="portarias.dat"
 
+
+# Para cada arquivo na pasta atual
 for file in *
 do
+	# Se o arquivo for .txt
     if echo "$file" | grep ".txt" > /dev/null
     then
-	if file "$file" | grep "ISO-8859" > /dev/null
-	then
-	    iconv --from-code=ISO-8859-15 --to-code=UTF-8 "$file" >> temp && mv temp "$file"
-	fi
+    	# Se o arquivo estiver com a codificação padrão, mudar para UTF-8
+		if file "$file" | grep "ISO-8859" > /dev/null
+		then
+	    	iconv --from-code=ISO-8859-15 --to-code=UTF-8 "$file" >> temp && mv temp "$file"
+		fi
 
+	#remove o .txt do nome do arquivo
 	nomeReal=$(echo "$file" | sed s/"\.txt"/""/)
+	
 	echo -n "$nomeReal|" >> "$arquivoDat"
 
-        cat "$file" | sed -n -e "/PORTARIAS/,$"p | \
+	# faz as transformacoes necessarias no texto
+    cat "$file" | sed -n -e "/PORTARIAS/,$"p | \
                       sed -e s/'PORTARIAS'/''/ \
                           -e s/"[áÁàÀâÂãÃ]"/"a"/g \
                           -e s/"[éÉêÊ]"/"e"/g \
@@ -28,6 +40,8 @@ do
 	echo -ne "|\n" >> "$arquivoDat"
    fi
 done
+
+#imprime o numero de entradas na primeira linha do arquivo
 
 numeroLinhas=$(wc -l "$arquivoDat" | sed s/"[^0-9]"/""/g)
 sed 1i\ "$numeroLinhas" "$arquivoDat" > temp && mv temp "$arquivoDat"
