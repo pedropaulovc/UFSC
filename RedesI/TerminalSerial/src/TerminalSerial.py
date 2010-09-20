319065bb0cc34ba323856a9896e672836decccdb
Pedro Paulo <pedropaulovc@gmail.com>
2010-09-20 02:03:04 +0000

    Código documentado

--- RedesI/TerminalSerial/src/TerminalSerial.py
+++ RedesI/TerminalSerial/src/TerminalSerial.py
@@ -5,15 +5,32 @@ from Getch import getch
 import sys
 
 def main():
+    """
+    Função incial do programa. Invoca
+    uma função que introduz o usuario ao
+    programa e inicia a conexão.
+    """
     exibirIntroducao()
     iniciarConexao()
 
 def exibirIntroducao():
+    """
+    Exibe uma pequena introdução ao usuário do programa
+    """
+    
     print "INE5414 - REDES DE COMPUTADORES I"
     print "PEDRO PAULO VEZZÁ CAMPOS"
     print "TRABALHO PRÁTICO 1: INTERCONEXÃO DE COMPUTADORES USANDO RS-232\n"
 
 def iniciarConexao():
+    """
+    Função principal do programa. Questiona o usuário das
+    configurações desejadas, inicia a conexão com a porta
+    serial definida e inicia duas threads: Uma responsável
+    por ler os caracteres escritos pelo usuário e outra 
+    responsável por escrever os caracteres recebidos do
+    terminal remoto.
+    """
     print "Forneça a porta serial a conexao utilizada: "
     porta = raw_input()
     if porta.startswith("CNC"):
@@ -36,6 +53,11 @@ def iniciarConexao():
     conexao.close
     
 def geraValorDefault(valor, default):
+    """
+    Gera um inteiro parseado de valor se valor 
+    é diferente de vazio senão retorna o valor
+    passado em default.
+    """
     if valor == "":
         valor = default
     else:
@@ -43,15 +65,28 @@ def geraValorDefault(valor, default):
     return valor
 
 class Sender(Thread):
+    """
+    Classe responsável por realizar um polling
+    capturando uma letra por vez, exibindo-a 
+    ao usuário e enviando-a via
+    interface serial.
+    """
     def __init__(self, conexao):
         Thread.__init__(self)
         self.conexao = conexao
         
     def run(self):
         while True:
-            self.conexao.write(getch())
+            ch = getch()
+            sys.stdout.write(ch)
+            self.conexao.write(ch)
 
 class Receiver(Thread):
+    """
+    Classe responsável por realizar um polling
+    lendo uma letra por vez da interface serial
+    e exibindo-a ao usuário.
+    """
     def __init__(self, conexao):
         Thread.__init__(self)
         self.conexao = conexao
