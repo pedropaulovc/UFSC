34c1a539d8c691298a827de4f23074acd99ea569
Pedro Paulo <pedropaulovc@gmail.com>
2010-11-21 15:23:02 +0000

    Implementados testes unitários

--- RedesI/TP4-ProtocoloTransporte/src/testes.py
+++ RedesI/TP4-ProtocoloTransporte/src/testes.py
@@ -12,7 +12,6 @@ from camadaAplicacao import CamadaAplicacao
 
 class Test(unittest.TestCase):
 
-
     def setUp(self):
         self.camRede = CamadaRede()
         self.camTransOrigem = CamadaTransporte(1, self.camRede)
@@ -21,6 +20,8 @@ class Test(unittest.TestCase):
         self.camRede.destino = self.camTransDestino
         self.camAplicOrigem = CamadaAplicacao(1, self.camTransOrigem)
         self.camAplicDestino = CamadaAplicacao(2, self.camTransDestino)
+        self.camAplicOrigem.aplicacao= self
+        self.camAplicDestino.aplicacao = self
         self.camTransOrigem.camadaAplicacao = self.camAplicOrigem
         self.camTransDestino.camadaAplicacao = self.camAplicDestino
         self.camAplicDestino.start()
@@ -35,8 +36,36 @@ class Test(unittest.TestCase):
         self.camAplicDestino = None
 
 
-    def testName(self):
-        pass
+    def testeConexaoOrigemDestino(self):
+        self.camAplicOrigem.conectar(2)
+        self.assertTrue(self.camAplicOrigem.cid > 0)
+        self.assertTrue(self.camAplicDestino.cid > 0)
+        
+    def testeConexaoDestinoOrigem(self):
+        self.camAplicDestino.conectar(1)
+        self.assertTrue(self.camAplicOrigem.cid > 0)
+        self.assertTrue(self.camAplicDestino.cid > 0)
+        
+    def testeEnviarPacoteOrigemDestino(self):
+        self.camAplicOrigem.conectar(2)
+        self.camAplicOrigem.enviarMensagem("Teste")
+        self.assertEquals(("Teste",2), self.msgRecebida)
+    
+    def testeEnviarPacoteDestinoOrigem(self):
+        self.camAplicDestino.conectar(1)
+        self.camAplicDestino.enviarMensagem("Teste")
+        self.assertEquals(("Teste",1), self.msgRecebida)
+    
+    def testeDesconectarOrigem(self):
+        self.camAplicOrigem.conectar(2)
+        self.assertEquals(0, self.camAplicOrigem.fecharConexao())
+    
+    def testeDesconectarDestino(self):
+        self.camAplicDestino.conectar(1)
+        self.assertEquals(0, self.camAplicOrigem.fecharConexao())
+        
+    def receberMensagem(self, msg, id):
+        self.msgRecebida = (msg, id)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
