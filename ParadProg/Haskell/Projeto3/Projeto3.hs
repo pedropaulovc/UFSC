fbfcc088ab94e4ed586ba5983d8a57757903e600
Pedro Paulo <pedropaulovc@gmail.com>
2010-11-26 17:52:57 +0000

    Primeira versão para entrega dos projetos finais de Haskell

--- ParadProg/Haskell/Projeto3/Projeto3.hs
+++ ParadProg/Haskell/Projeto3/Projeto3.hs
@@ -1,38 +1,46 @@
 import IO
 import Char
 import List
-import System (getArgs)
+import Control.Monad
+import System (getArgs, exitFailure)
 
 main :: IO ()
 main = do
 	args <- getArgs
 	case args of
-		[nomeArquivo] -> do	
+		[nomeArquivo] -> do
+			-- Lendo o arquivo fornecido
 			arquivoStr <- readFile nomeArquivo
-		
-			let nomesArquivosRGB = ["saidaR.txt", "saidaG.txt", "saidaB.txt"]
+			
+			-- Verificando se o arquivo realmente é uma imagem BMP
+			when (not(ehBMP arquivoStr)) $ erro "Imagem nao e do formato BMP\n"
+			
+			-- Gerando os nomes dos arquivos de resultados
+			let nome = take (length nomeArquivo -4) nomeArquivo
+			let nomesArqsRGB = map (nome ++) ["-R.txt", "-G.txt", "-B.txt"]
+			
+			-- Abrindo os arquivos de resultados para escrita
+			arquivosRGB <- mapM abrirEscrita nomesArqsRGB
 
-			--abrindo os arquivos para escrita
-			arquivosRGB <- mapM abrirEscrita nomesArquivosRGB
-
-			--separando o cabeçalho da imagem
-			let (cabecalho, imagem) = obterCabecalho arquivoStr
+			-- Separando o cabeçalho do restante imagem
+			let (cabecalho, corpoImagem) = obterCabecalho arquivoStr
 			let largura = obterLargura cabecalho
-			let imagemSemPadding = removePadding imagem largura
 			
-			--a partir da string de bytes obter a lista de pixels da imagem
-			let pixels = gerarTriplas imagemSemPadding
+			-- Removendo o padding da imagem BMP
+			let imagemSemPadding = removerPadding corpoImagem largura
 			
-			--a transposição é o núcleo da separação em camadas. De uma lista de
-			--pixels (Triplas), temos três listas de strings, cada uma contendo os
-			--bytes de uma cor
-			--arquivos bmp são codificados como bgr, a reversão gera o rgb.
+			-- A partir da string de bytes resultante obter os pixels da imagem
+			let pixels = gerarPixels imagemSemPadding
+
+			-- Ao transpor os pixels (Triplas BGR), gera-se três strings,
+			-- cada uma contendo os bytes de uma cor.
+			-- Ao reverter o bgr obtem-se o rgb
 			let rgb = reverse (transpose pixels)
 
-			--gravar os dados obtidos em cada arquivo
-			sequence [hPrint arq byte | (arq, byte) <- zip arquivosRGB rgb]
+			-- Gravando os dados obtidos em cada arquivo
+			sequence [gravarCor arq cor | (arq, cor) <- zip arquivosRGB rgb]
 
-			--fecha os arquivos
+			-- Fechando os arquivos
 			mapM hClose arquivosRGB
 
 			putStr "Fim.\n"
@@ -41,29 +49,47 @@ main = do
 				putStr "Forneca a imagem a ser decomposta como parametro:\n"
 				putStr "Uso: ./decompor_imagem imagem.bmp\n"
 
+-- Abre um dado arquivo para escrita e retorna o Handle correspondente.
 abrirEscrita :: [Char] -> IO Handle
 abrirEscrita arquivo = openFile arquivo WriteMode
 
+-- Verifica se o início do header corresponde a um de uma imagem BMP.
+ehBMP :: [Char] -> Bool
+ehBMP arquivo = take 2 arquivo == "BM"
+
+-- Separa o cabeçalho da imagem do restante, retorna ambos em uma dupla.
 obterCabecalho :: [Char] -> ([Char],[Char])
 obterCabecalho [] = ([],[])
 obterCabecalho imagem = splitAt 54 imagem
 
-gerarTriplas :: [Char] -> [ [Char] ]
-gerarTriplas [] = []
-gerarTriplas arquivo = (take 3 arquivo):(gerarTriplas (drop 3 arquivo) )
+-- A partir de uma imagem sem padding pega de 3 em 3 bytes, gerando um pixel 
+gerarPixels :: [Char] -> [ [Char] ]
+gerarPixels [] = []
+gerarPixels arquivo = (take 3 arquivo):(gerarPixels (drop 3 arquivo) )
 
--- Funções para obter as dimenções da imagem a partir do cabeçalho
+-- A partir do cabeçalho da imagem obtém a largura em pixels da imagem.
 obterLargura :: [Char] -> Int
 obterLargura [] = 0
 obterLargura arquivoStr = let lista = map ord (take 4 (drop 18 arquivoStr))  in  (lista!!0) + (lista!!1) * 256
 
 
--- Funções que removem e adicionam os bytes nulos de padding, para que cada linha
--- da imagem tenha um número de bytes múltiplo de 4
-removePadding :: [Char] -> Int -> [Char]
-removePadding [] _ = []
-removePadding imagem largura = let (atual, resto) = splitAt (largura*3) imagem
-				   pads = mod (largura*3) 4  in
+-- Removem os bytes nulos de alinhamento (padding) com palavras de 32 bits,
+-- i.e. um byte a cada pixel. 
+removerPadding :: [Char] -> Int -> [Char]
+removerPadding [] _ = []
+removerPadding imagem largura = let (atual, resto) = splitAt (largura*3) imagem
+				    pads = mod (largura*3) 4  in
 					let proximasLinhas = drop pads resto  in
-						atual ++ (removePadding proximasLinhas largura)
+						atual ++ (removerPadding proximasLinhas largura)
+
+-- A partir de uma string correspondente a uma única cor, a transforma em uma lista
+-- de números e grava no arquivo fornecido.
+gravarCor :: Handle -> [Char] -> IO ()
+gravarCor arquivo cor = hPrint arquivo (map ord cor)
+
+-- Exibe uma mensagem de erro ao usuário e termina a execução do programa prematuramente
+erro :: String -> IO a
+erro mensagem = do
+          putStr mensagem
+          exitFailure
 
