f1cf24fb5fcdc2fc6f8e77e49e41fa0009bc5e3b
Pedro Paulo <pedropaulovc@gmail.com>
2010-12-07 18:25:39 +0000

    Segunda versão para entrega dos projetos finais de Haskell. Todos projetos com main e interface texto.

--- ParadProg/Haskell/Projeto1/Projeto1.hs
+++ ParadProg/Haskell/Projeto1/Projeto1.hs
@@ -1,7 +1,14 @@
-module Solidos  ( Solido ( Esfera, Cilindro, Cone, TroncoCone, EsferoideOblato, EsferoideProlato),
+-- INE5416 - PARADIGMAS DE PROGRAMACAO
+-- ALUNO: PEDRO PAULO VEZZA CAMPOS - 09132033
+-- PROJETO FINAL 1: AREA E VOLUME DE SOLIDOS
+
+module Main  ( Solido ( Esfera, Cilindro, Cone, TroncoCone, EsferoideOblato, EsferoideProlato),
 				 Raio, Altura, SemiEixo,
 				 excentricidade
 				) where
+import IO
+import System.Exit
+
 data Solido = Esfera Raio
 			| Cilindro Raio Altura
 			| Cone Raio Altura
@@ -13,18 +20,99 @@ type Raio = Float
 type Altura = Float
 type SemiEixo = Float
 
+-----------------------------------------------------
+---------------- main e interface -------------------
+-----------------------------------------------------
+
+main :: IO ()
+main = entrada
+entrada = do
+ 	putStrLn "\nDigite a opcao desejada:"
+	putStrLn"1) Calcular area e volume de uma esfera."
+	putStrLn"2) Calcular a area lateral, total e volume de um cilindro."
+	putStrLn"3) Calcular a area lateral, total e volume de um cone."
+	putStrLn"4) Calcular a area lateral, total e volume de um tronco de cone."
+	putStrLn"5) Calcular a area e volume de um esferoide oblato."
+	putStrLn"6) Calcular a area e volume de um esferoide prolato."
+	putStrLn"7) Encerrar."
+	x<-getLine 
+	case x of 
+		'1': _ -> do	print("Digite o raio da esfera.")
+				raio<-getLine
+				print("Area = "++show(areaTotal (Esfera(read raio))))
+				print("Volume = "++show(volume (Esfera(read raio))))
+				entrada	
+	
+		'2': _ -> do	print("Digite o raio do cilindro.")
+				raio<-getLine
+				print("Digite a altura do cilindro.")
+				altura<-getLine
+				print("Area lateral = "++show(areaLateral (Cilindro (read raio) (read altura))))
+				print("Area total = "++show(areaTotal (Cilindro(read raio) (read altura))))
+				print("Volume = "++show(volume (Cilindro (read raio)(read altura))))
+				entrada	
+
+		'3': _ -> do	print("Digite o raio do cone.")
+				raio<-getLine
+				print("Digite a altura do cone.")
+				altura<-getLine
+				print("Area lateral = "++show(areaLateral (Cone(read raio)(read altura))))
+				print("Area total = "++show(areaTotal (Cone(read raio)(read altura))))
+				print("Volume = "++show(volume (Cone(read raio)(read altura))))
+				entrada	
+
+		'4': _ -> do	print("Digite o raio menor do tronco de cone.")
+				raioMenor<-getLine
+				print("Digite o raio maior do tronco de cone.")
+				raioMaior<-getLine
+				print("Digite a altura do tronco de cone.")
+				altura<-getLine
+				print("Area lateral = "++show(areaLateral (TroncoCone(read raioMenor) (read raioMaior) (read altura))))
+				print("Area total = "++show(areaTotal (TroncoCone(read raioMenor) (read raioMaior) (read altura))))
+				print("Volume = "++show(volume (TroncoCone(read raioMenor) (read raioMaior) (read altura))))
+				entrada
+
+		'5': _ -> do	print("Digite o valor do semi-eixo menor.")
+				semiMenor<-getLine
+				print("Digite o valor do semi-eixo maior.")
+				semiMaior<-getLine
+				print("Area = "++show(areaTotal (EsferoideOblato(read semiMenor)(read semiMaior))))
+				print("Volume = "++show(volume (EsferoideOblato(read semiMenor)(read semiMaior))))
+				entrada	
+
+		'6': _ -> do	print("Digite o valor do semi-eixo menor.")
+				semiMenor<-getLine
+				print("Digite o valor do semi-eixo maior.")
+				semiMaior<-getLine
+				print("Area = "++show(areaTotal (EsferoideProlato(read semiMenor)(read semiMaior))))
+				print("Volume = "++show(volume (EsferoideProlato(read semiMenor)(read semiMaior))))
+				entrada	
+
+		'7' : _ -> do	exitSuccess
+		_: _ -> do	print("Opcao invalida")			
+
+-----------------------------------------------------
+---------------- código da aplicacao ----------------
+-----------------------------------------------------
+
 excentricidade :: Float -> Float -> Float
 excentricidade a b = (sqrt(a*a - b*b))/a
 
-area ::  Solido -> Float
-area (Esfera r) = 4 * pi * r * r
-area (Cilindro r h) = (2 * pi * r * h) + (2 * pi * r * r)
-area (Cone r h) = pi * r * ((sqrt r * r + h * h) + r )
-area (TroncoCone rMenor rMaior h) = pi * rMenor * rMenor + pi * rMaior * rMaior + pi * (rMenor + rMaior)*(sqrt h * h + rMenor * rMenor + rMaior * rMaior -2 * rMenor * rMaior)
-area (EsferoideOblato seMenor seMaior) = let e = excentricidade seMenor seMaior
-			      						 in 2 * pi * seMenor * seMenor + pi * ((seMaior * seMaior)/e) * (log (1 + e)/(1 - e))
-area (EsferoideProlato seMenor seMaior) = let e = excentricidade seMenor seMaior
-			      						  in 2 * pi * seMaior * seMaior + 2 * pi * (seMenor * seMaior/e) * (asin e)
+areaTotal ::  Solido -> Float
+areaTotal (Esfera r) = 4 * pi * r * r
+areaTotal (Cilindro r h) = (2 * pi * r * h) + (2 * pi * r * r)
+areaTotal (Cone r h) = pi * r * ((sqrt r * r + h * h) + r )
+areaTotal (TroncoCone rMenor rMaior h) = pi * rMenor * rMenor + pi * rMaior * rMaior + pi * (rMenor + rMaior)*(sqrt h * h + rMenor * rMenor + rMaior * rMaior -2 * rMenor * rMaior)
+areaTotal (EsferoideOblato seMenor seMaior) = let e = excentricidade seMaior seMenor
+			      							  in 2 * pi * seMenor * seMenor + pi * ((seMaior * seMaior)/e) * (log (1 + e)/(1 - e))
+
+areaTotal (EsferoideProlato seMenor seMaior) = let e = excentricidade seMaior seMenor
+			      							   in 2 * pi * seMaior * seMaior + 2 * pi * (seMenor * seMaior/e) * (asin e)
+
+areaLateral :: Solido -> Float
+areaLateral (Cilindro r h) = 2 * pi * r * h
+areaLateral (Cone r h) = pi * r * (sqrt r * r + h * h)
+areaLateral (TroncoCone rMenor rMaior h) = pi * (rMenor + rMaior)*(sqrt h * h + rMenor * rMenor + rMaior * rMaior - 2 * rMenor * rMaior)
 
 volume :: Solido -> Float
 volume (Esfera r) = (4/3) * pi * r * r * r
--- ParadProg/Haskell/Projeto2/Projeto2.hs
+++ ParadProg/Haskell/Projeto2/Projeto2.hs
@@ -1,27 +1,110 @@
-module Series (somaInt, totalInt, somaImpares, totalImpares, somaPares, totalPares,
+-- INE5416 - PARADIGMAS DE PROGRAMACAO
+-- ALUNO: PEDRO PAULO VEZZA CAMPOS - 09132033
+-- PROJETO FINAL 2: SERIES
+
+module Main (somaInt, totalInt, somaImpares, totalImpares, somaPares, totalPares,
 			  somaQuadrados, totalQuadrados, somaQuadradosImpares,
 			  totalQuadradosImpares, quaseUm, quaseDois, fat, quaseE) where
 
+import IO
+import System.Exit
+
+-----------------------------------------------------
+---------------- main e interface -------------------
+-----------------------------------------------------
+
+main :: IO ()
+main = entrada
+entrada = do
+ 	putStrLn "Digite o numero da opcao desejada:"
+	putStrLn "1) Para soma de uma lista de inteiros"
+	putStrLn "2) Para soma de uma lista de inteiros impares"
+	putStrLn "3) Para soma de uma lista de inteiros pares"
+	putStrLn "4) Para soma de uma lista de quadrados de inteiros"
+	putStrLn "5) Para soma de uma lista de quadrados de inteiros impares"
+	putStrLn "6) Para soma infinita quase 1."
+	putStrLn "7) Para soma infinita quase e."
+	putStrLn "8) Para soma infinita quase 2."
+	putStrLn "9) Para sair."
+	x<-getLine 
+	
+	print("Digite o numero de elementos da lista.")
+	elemento<-getLine
+
+	case x of 
+		'1': _ -> do			
+				print("A soma de uma lista de "++elemento++" inteiros = "++show(totalInt (read elemento)))
+				entrada	
+		
+		'2': _ -> do			
+				print("A soma de uma lista de "++elemento++" inteiros impares = "++show(totalImpares (read elemento)))
+				entrada	
+
+		'3': _ -> do				
+				print("A soma de uma lista de "++elemento++" inteiros pares = "++show(totalPares (read elemento)))
+				entrada	
+		
+		'4': _ -> do				
+				print("A soma de uma lista de "++elemento++" quadrados de inteiros = "++show(totalQuadrados (read elemento)))
+				entrada	
+		
+		'5': _ -> do				
+				print("A soma de uma lista de "++elemento++" quadrados de inteiros impares = "++show(totalQuadradosImpares (read elemento)))
+				entrada	
+
+		'6': _ -> do			
+				print("O numero quase um = "++show(quaseUm (read elemento)))
+				entrada	
+
+		'7': _ -> do				
+				print("O numero quase e = "++show(quaseE (read elemento)))
+				entrada	
+
+		'8': _ -> do				
+				print("O numero quase dois = "++show(quaseDois (read elemento)))
+				entrada	
+
+		'9' : _ -> do	exitSuccess
+
+		_: _ -> do	print("Opcao invalida")		
+
+-----------------------------------------------------
+---------------- código da aplicação ----------------
+-----------------------------------------------------
+
+somaInt :: (Fractional a, Enum a) => a -> [a]
 somaInt n = map (\x->(x*(1+x)/2)) [1..n]
+totalInt :: (Fractional a, Enum a) => a -> a
 totalInt n = last (somaInt n)
 
+somaImpares :: Int -> [Int]
 somaImpares n = map(\x->x * x) [1..n]
+totalImpares :: Int -> Int
 totalImpares n = last (somaImpares n)
 
+somaPares :: (Fractional a, Enum a) => a -> [a]
 somaPares n = map(\x->x * (x - 1)) [1..n]
+totalPares :: (Fractional a, Enum a) => a -> a
 totalPares n = last (somaPares n)
 
+somaQuadrados :: (Fractional a, Enum a) => a -> [a]
 somaQuadrados n = map(\x->x * (x + 1) * (2 * x + 1)/6) [1..n]
+totalQuadrados :: (Fractional a, Enum a) => a -> a
 totalQuadrados n = last (somaQuadrados n)
 
+somaQuadradosImpares :: (Fractional a, Enum a) => a -> [a]
 somaQuadradosImpares n = map(\x->x * (4 * x * x - 1)/3) [1..n]
+totalQuadradosImpares :: (Fractional a, Enum a) => a -> a
 totalQuadradosImpares n = last (somaQuadradosImpares n)
 
+quaseUm :: Int -> Double
 quaseUm n = sum( map( \x->( 1/(x * x + x) )) (take n [1..]) )
 
+quaseDois :: Int -> Double
 quaseDois n = sum( map( \x->( 2/(x * x + x) )) (take n [1..]) )
 
 fat 0 = 1
 fat n = n * fat (n - 1)
 
-quaseE n = sum( map( \x->( 1/(fat x) ) ) [0..n] )
+quaseE :: Int -> Double
+quaseE n = sum( map( \x->( 1/(fat x) ) ) (take n [0..]) )
